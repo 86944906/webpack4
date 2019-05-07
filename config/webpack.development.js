d29a3d75c3ed62b8c88b65517bbfd740cb4737ee
zhouxiang <zhouxiang@300.cn>
2019-05-07 14:11:11 +0800

    '部分修改'

--- config/webpack.development.js
+++ config/webpack.development.js
@@ -10,13 +10,21 @@ module.exports = {
             //多入口文件配置[name],生成js文件随入口文件名字，如是单入口直接写bundle.js
             filename:'[name].js'
         },
-        
-        devtool:'source-map'
+        devtool:'source-map',
+        devServer:{
+            port:3002,
+            open:true,
+        },
+        proxy:{
+            '^/api':{
+                target:'http://localhost:5000',
+                pathRewrite:{
+                    '^/api':'/'
+                }
+            }
+        }
     },
     plugins:[
-        /* new HtmlWebpackPlugin({
-            template:path.resolve(__dirname,'../index.html')
-        }), */
         new MiniCssExtractPlugin({
             filename: '[name]-[contenthash].css'
         })
